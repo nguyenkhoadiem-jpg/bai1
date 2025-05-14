1ebec97a673bad5b15aad448159e6d9f64e28861
nguyenkhoadiem-jpg <119786360+nguyenkhoadiem-jpg@users.noreply.github.com>
2025-05-14 14:15:10 +0700

    Add files via upload

---
+++ bai1.py
@@ -0,0 +1,17 @@
+# Ngày 1: Tự động hóa quy trình
+
+# Khai báo biến first_name, FIRST_NAME và gán giá trị cho nó
+first_name = "An"
+FIRST_NAME = "AN"
+
+
+a = 5
+b = 7
+tong = a + b
+print("Tổng của hai số là:", tong)
+
+import math
+
+ban_kinh = 30
+dien_tich = math.pi * ban_kinh**2
+print("Diện tích hình tròn là:", dien_tich)
